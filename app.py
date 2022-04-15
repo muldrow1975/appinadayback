96040acecdbd3cac26a9d1f1c17be45b0af68bf9
8U2N <tylerburnworthwriting@gmail.com>
2022-04-15 10:57:55 -0700

    added user class to app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import os
 
@@ -7,6 +9,24 @@ CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_name = db.Column(db.String, Unique=False, nullable=False)
+    password = db.Column(db.String, Unique=False, nullable=False)
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'user_name', 'password')
+
+user_schema = UserSchema()
+multi_user_schema = UserSchema()
 
 @app.route('/room1', methods=["GET"])
 def Choice_one_page():
